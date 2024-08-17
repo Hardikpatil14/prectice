fc69f53add0fafbcaeed44034ed6dc87ee7c40bb
Hardik <patilhardikmaheshbhai@gmail.com>
2024-08-17 16:31:54 +0530

    sql commit

---
+++ MySQL/notes.sql
@@ -0,0 +1,17 @@
+-- -- for creating databse
+-- -- create database <db-name>; 
+-- create database mlp;
+-- show databases
+
+-- -- for creating tablse
+-- -- create table <table-name>(<col1-name> <datatype>(size),<col2-name> <datatype>(size)<col3-name> <datatype>(size)..)
+-- create table student(id int, name varchar(20),contact numeric(10),address varchar(20),emailid varchar(20));
+
+-- show tables
+
+-- insert records
+-- insert into student values(1001,'Raj',9898989898,'surat','raj@gmail.com');
+
+-- for retrive table 
+-- select * from <table-name>;
+-- select * from student;
