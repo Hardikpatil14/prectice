be94f54cd7beb34e73f21d39bb7cec0ae61cc483
Hardik <patilhardikmaheshbhai@gmail.com>
2024-08-18 11:48:12 +0530

    commit

--- MySQL/notes.sql
+++ MySQL/notes.sql
@@ -21,7 +21,7 @@ create table client_master(client_no int,name varchar(20),city varchar(10),pinco
 
 create table product_master(pro_no int,Description varchar(10),profit_percent int,unit_measure varchar(10),qty_on_hand int,reorderLv1 int,sell_price int,cost_price int);
 
-create table salesman_master(salesman_no int,name varchar(10),add1 varchar(15),add2 varchar(15),city varchar(10),pincode int,state varchar(15),salAmt int,TgtToGet int,YtdSales int,Remarks int);
+create table salesman_master(salesman_no varchar(10),name varchar(10),add1 varchar(15),add2 varchar(15),city varchar(10),pincode int,state varchar(15),salAmt int,TgtToGet int,YtdSales int,Remarks varchar(10));
 
 insert into client_master values(0001,'lvan Byross','Mumbai',400054,'maharashtr',15000);
 insert into client_master values(0002,'Mamta Muzumdar','Mumbai',780001,'maharashtr',0);
@@ -43,4 +43,8 @@ insert into product_master values(07965,'denimshirt',4,'piece',100,40,350,250);
 insert into product_master values(07975,'Lycra Tops',5,'piece',70,30,300,175);
 insert into product_master values(08865,'Skirts',5,'piece',75,30,450,300);
 
-
+-- table three
+insert into salesman_master values(0001,'Aman','A/14','Worli','Mumbai',400002,'maharashtr',3000,100,50,'Good');
+insert into salesman_master values(0002,'omkar','65','Nariman','Mumbai',400001,'maharashtr',3000,200,100,'Good');
+insert into salesman_master values(0002,'omkar','65','Nariman','Mumbai',400001,'maharashtr',3000,200,100,'Good');
+insert into salesman_master values();
